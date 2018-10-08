13467bf6e241be06d6f9550cf8d0283b562d6048
Liwen Jiang <tomjlw123@gmail.com>
2018-10-07 20:32:14 -0500

    Create test.py

---
+++ src/test.py
@@ -0,0 +1,10 @@
+import os
+import sys
+sys.path.append("..")
+import lib
+
+filepath = "../Data/chorales.csv"
+
+# first normalize pitches
+IDs, sts, pitchs, durs, keysigs, timesigs, fermatas = lib.str2list(filepath)
+lib.standarization(pitchs, keysigs)
